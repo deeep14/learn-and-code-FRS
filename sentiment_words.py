161c44492353f69ee97450e3ee74b7ed306c691f
Deepak Masuti <deepak.s.masuti@intimetec.com>
2024-06-21 04:37:07 +0530

    Adding files for sentiment analysis

---
+++ sentiment_words.py
@@ -0,0 +1,10 @@
+positive_words = [
+    "good", "great", "excellent", "amazing", "wonderful", "love", "like", "positive", "fantastic", "best", "awesome", "nice",
+    "delicious", "crispy", "perfectly", "amazing", "tasty", "healthy", "refreshing", "yummy", "satisfying", "savory", "delectable",
+    "flavorful", "mouthwatering", "appetizing", "palatable", "scrumptious"
+]
+
+negative_words = [
+    "bad", "terrible", "awful", "horrible", "worst", "hate", "dislike", "negative", "poor", "sad", "angry", "spicy",
+    "oily", "salty", "bland", "burnt", "expensive", "unavailable", "stale", "cold", "overcooked", "undercooked", "greasy", "unpalatable"
+]

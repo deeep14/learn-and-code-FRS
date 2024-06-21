529012988beae21c2a6da6336e1270918b4dcc80
Deepak Masuti <deepak.s.masuti@intimetec.com>
2024-06-21 09:10:38 +0530

    updated feedback

--- sentiment_words.py
+++
@@ -1,7 +0,0 @@
-positive_words = [
-    "good", "great", "excellent", "amazing", "wonderful", "love", "like", "positive"
-]
-
-negative_words = [
-    "bad", "terrible", "awful", "horrible", "worst", "hate", "dislike", "negative"
-]
